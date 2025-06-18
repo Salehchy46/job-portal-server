11636256a24910a765ff65c702c373dbffa5f0ac
salehchy46 <salehchyctg@gmail.com>
2025-06-18 16:02:01 +0600

    job post added to the server side

--- index.js
+++ index.js
@@ -46,6 +46,12 @@ async function run() {
             res.send(result);
         })
 
+        app.post('/jobs', async(req, res) => {
+            const newJob = req.body;
+            const result = await jobCollection.insertOne(newJob);
+            res.send(result);
+        })
+
         // job application apis
 
         app.get('/job-application', async (req, res) => {
