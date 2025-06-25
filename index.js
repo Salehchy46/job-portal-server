08a612d05b6c981d88d471f1ef1dbfb843e86b56
salehchy46 <salehchyctg@gmail.com>
2025-06-26 02:30:08 +0600

    token is sent to cookie

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 const jwt = require('jsonwebtoken');
+const cookieParser = require('cookie-parser');
 const app = express();
 require('dotenv').config();
 const port = process.env.PORT || 3000;
@@ -9,6 +10,7 @@ const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 
 app.use(express.json());
 app.use(cors());
+app.use(cookieParser());
 
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.vu0s8qh.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
@@ -37,8 +39,13 @@ async function run() {
 
         app.post('/jwt', async(req, res) => {
             const user = req.body;
-            const token = jwt.sign(user, 'secret', {expiresIn: '1hr'});
-            res.send(token);
+            const token = jwt.sign(user, process.env.JWT_SECRET, {expiresIn: '1hr'});
+            res
+            .cookie('token', token, {
+                httpOnly: true,
+                secure: false
+            })
+            .send({success: true});
         })
 
         app.get('/jobs', async (req, res) => {
